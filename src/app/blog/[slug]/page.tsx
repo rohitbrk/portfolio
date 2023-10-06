7443fb89d64785b3bcd232f704588a1cf28ec0b1
Rohith <rohithkumarbathula99@gmail.com>
2023-10-06 19:36:54 +0530

    add blog next-mdx-remote

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,15 +1,46 @@
-import ReactJs from "../../../blogs/ReactJs.mdx";
-import NodeJs from "../../../blogs/NodeJs.mdx";
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+
+import { MDXRemote } from "next-mdx-remote/rsc";
+
+export async function generateStaticParams() {
+  const _path = "src/app/blogs";
+  const files = fs.readdirSync(path.join(_path));
+
+  const paths = files.map((filename) => ({
+    slug: filename.replace(".mdx", ""),
+  }));
+
+  return paths;
+}
+
+function getPost({ slug }: { slug: string }) {
+  const _path = "src/app/blogs";
+
+  const markdownFile = fs.readFileSync(
+    path.join(_path, slug + ".mdx"),
+    "utf-8"
+  );
+
+  const { data: fontMatter, content } = matter(markdownFile);
+
+  return {
+    fontMatter,
+    slug,
+    content,
+  };
+}
 
 const Page = ({ params }) => {
-  let ComponentToRender = null;
-  if (params.slug === "react-js") ComponentToRender = ReactJs;
-  if (params.slug === "node-js") ComponentToRender = NodeJs;
+  const props = getPost(params);
 
   return (
-    <div className="prose dark:prose-invert text-white mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl m-20">
-      <ComponentToRender />
-    </div>
+    <article className="prose prose-sm md:prose-base lg:prose-lg prose-slate dark:prose-invert mx-auto">
+      <h1>{props.fontMatter.title}</h1>
+
+      <MDXRemote source={props.content}></MDXRemote>
+    </article>
   );
 };
 
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,42 +1,47 @@
-const blogs = [
-  {
-    id: 1,
-    title: "What is ReactJs?",
-    imgUrl: "/../reactjs-logo.jpg",
-    desc: "A brief introduction to ReactJs - The UI library - Frontend Engineering",
-  },
-  {
-    id: 2,
-    title: "How NodeJs works?",
-    imgUrl: "/../nodejs-logo.jpg",
-    desc: "Introduction to Backend Engineering - NodeJs",
-  },
-];
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+import Link from "next/link";
 
 const Blog = () => {
+  const blogDir = "src/app/blogs";
+  const files = fs.readdirSync(path.join(blogDir));
+
+  const blogs = files.map((filename) => {
+    const fileContent = fs.readFileSync(path.join(blogDir, filename), "utf-8");
+
+    const { data } = matter(fileContent);
+    return {
+      slug: filename.replace(".mdx", ""),
+      imgUrl: data.imgUrl,
+      title: data.title,
+      desc: data.desc,
+    };
+  });
+
   return (
     <main className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl m-20">
       <div className="p-2 flex flex-col gap-1 justify-center items-center">
         {" "}
         {blogs.map((item) => (
           <div
-            key={item.id}
+            key={item.slug}
             className="max-w-md bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
           >
-            <a href="#">
+            <Link href={`blog/${item.slug}`}>
               <img className="rounded-t-lg" src={item.imgUrl} alt="" />
-            </a>
+            </Link>
             <div className="p-5">
-              <a href="#">
+              <Link href={`blog/${item.slug}`}>
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                   {item.title}
                 </h5>
-              </a>
+              </Link>
               <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
                 {item.desc}
               </p>
-              <a
-                href="#"
+              <Link
+                href={`blog/${item.slug}`}
                 className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-700 dark:hover:bg-blue-800 dark:focus:ring-blue-800"
               >
                 Read more
@@ -55,7 +60,7 @@ const Blog = () => {
                     d="M1 5h12m0 0L9 1m4 4L9 9"
                   />
                 </svg>
-              </a>
+              </Link>
             </div>
           </div>
         ))}

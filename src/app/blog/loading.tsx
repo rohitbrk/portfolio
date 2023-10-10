26d1f2872bf9627d10ba016879ecfb5ef2472714
Rohith <rohithkumarbathula99@gmail.com>
2023-10-10 21:48:05 +0530

    add loading.tsx

---
+++ src/app/blog/loading.tsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+  return (
+    <main className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl m-20">
+      <div className="p-2 flex flex-col gap-1 justify-center items-center">
+        <p>Loading...</p>
+      </div>
+    </main>
+  );
+}

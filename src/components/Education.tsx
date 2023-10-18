21dc7d8253a32b616701448794c4a962e79ead58
Rohith <rohithkumarbathula99@gmail.com>
2023-10-18 10:38:08 +0530

    fixed key prop error

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -1,17 +1,20 @@
 const education = [
   {
+    id: 1,
     course: "Bachelor of Technology",
     batch: "(2017 - 2021)",
     // grade: "7.02 CGPA",
     location: "JNTUACEP",
   },
   {
+    id: 2,
     course: "Intermediate",
     batch: "(2015 - 2017)",
     grade: "983/ 1000",
     location: "Narayana Junior College - ",
   },
   {
+    id: 3,
     course: "SSC",
     batch: "(2014 - 2015)",
     grade: "10 CGPA",
@@ -24,7 +27,10 @@ const Education = () => {
     <div className="flex flex-col justify-center items-center text-center mb-6">
       <h1 className="text-2xl font-bold mb-2">Education</h1>
       {education.map((item) => (
-        <div className="w-full max-w-xl m-1 px-6 py-2 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <div
+          key={item.id}
+          className="w-full max-w-xl m-1 px-6 py-2 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+        >
           <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
             {`${item.course} ${item.batch}`}
           </h5>
